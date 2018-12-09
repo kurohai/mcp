495d1a7d1683dbf285cd0c87e164eb5600768d4e
Evan Burt <evantburt@gmail.com>
2018-12-08 21:17:04 -0600

    changed host and port

--- mcp/app.py
+++ mcp/app.py
@@ -116,4 +116,8 @@ class DeviceList(Resource):
 if __name__ == '__main__':
     app = Flask(__name__)
     app.register_blueprint(api_v1)
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        port=8080,
+        host='0.0.0.0'
+    )
